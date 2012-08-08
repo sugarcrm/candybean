e37e73b30caa910c7ac8d7e1a3636ac1e238b865
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-08-07 19:36:23 -0700

    reorg & git logistics

---
+++ src/main/java/com/voodoo2/Automation.java
@@ -0,0 +1,63 @@
+package com.voodoo2;
+
+import java.awt.Toolkit;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Automation {
+	
+	public static String webElementToString(WebElement element) {
+		List<WebElement> childElements = element.findElements(By.xpath("*"));
+		String s = element.getTagName() + ":" + element.getText() + " ";
+		for(WebElement we : childElements) {
+			s += we.getTagName() + ":" + we.getText() + " ";
+		}
+		return s;
+	}
+	
+	public static void maximizeBrowserWindow(WebDriver browser) {
+		java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		browser.manage().window().setSize(new Dimension(screenSize.width, screenSize.height));
+	}
+	
+	public static void allOptionsAction(Select selectElement, WebElement actionElement) {
+		List<WebElement> options = selectElement.getOptions(); 
+		for(WebElement option : options) {
+			selectElement.selectByVisibleText(option.getText());
+			actionElement.click();
+		}
+	}
+	
+	public static void optionAction(Select selectElement, Set<String> actionOptionValues, WebElement actionElement) throws Exception {
+		List<WebElement> allOptions = selectElement.getOptions();
+		HashSet<String> optionValues = new HashSet<String>();
+		for(WebElement option : allOptions) {
+			optionValues.add(option.getText());
+//			System.out.println("Adding to options set:" + option.getText());
+		}
+		if(optionValues.containsAll(actionOptionValues)) {
+			for(String option : actionOptionValues) {
+				selectElement.selectByVisibleText(option);
+				actionElement.click();
+			}
+		} else throw new Exception("Specified select option unavailable...");
+	}
+	
+	public static void explicitWait(WebDriver browser, long timeout, final By by) throws Exception {
+		(new WebDriverWait(browser, timeout)).until(new ExpectedCondition<WebElement>(){
+			public WebElement apply(WebDriver wd) {
+				return wd.findElement(by);
+			}});
+	}
+	
+	public static getBrowsers
+}
