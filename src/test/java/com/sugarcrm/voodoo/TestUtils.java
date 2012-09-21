630a864705f2ab97a62b1cdb35f0faf0503b8ef5
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-09-21 13:27:31 -0700

    Renamed Selenium test to match; updating tests

--- src/test/java/com/sugarcrm/voodoo/TestUtils.java
+++ src/test/java/com/sugarcrm/voodoo/TestUtils.java
@@ -1,8 +1,13 @@
 package com.sugarcrm.voodoo;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -11,24 +16,46 @@ public class TestUtils {
 	
 	@Test
 	public void testGetCascadingPropertyValue() {
-		String propsFileName = "testutils.prop";
-		String propKey = "key";
-		String propConfigVal = "configvalue";
-		String propDefaultVal = "defaultvalue";
-		String propSysVal = "systemvalue";
-	
-		// Resource setup
-		File propsFile = new File(propsFileName);
+		try {
+			String propsFilePath = ".//src/test/resources/";
+			String propsFilePrefix = "testutils";
+			String propsFileSuffix = "_en_US.properties";
+			String propsFileName = propsFilePath + propsFilePrefix + propsFileSuffix;
+			String propKey = "key";
+			String propSysKey = "syskey";
+			String propConfigVal = "configvalue";
+			String propDefaultVal = "defaultvalue";
+			String propSysVal = "systemvalue";
 		
-		// Test
-//		BufferedWriter propsWriter = new BufferedWriter(new FileWriter(propsFile));
-//		propsWriter.write(propKey + " = " + propConfigVal);
-//		ResourceBundle rb = ResourceBundle.getBundle(propsFileName);
-//		String actualDefaultVal = Utils.getCascadingPropertyValue(rb, propsDefault, key);
-//		Assert.assertEquals("", expected, actual)
-		
-		// Resource cleanup
-		propsFile.delete();
+			// Resource setup
+//			File propsFile = new File(propsFileName);
+//			propsFile.createNewFile();
+//			BufferedWriter propsWriter = new BufferedWriter(new FileWriter(propsFile));
+//			propsWriter.write(propKey + " = " + propConfigVal + System.lineSeparator());
+//			propsWriter.write(propSysKey + " = " + propConfigVal + System.lineSeparator());
+//			propsWriter.flush();
+//			System.setProperty(propSysKey, propSysVal);
+//			ResourceBundle rb = ResourceBundle.getBundle(propsFilePrefix);
+			
+			// Test
+//			String actualDefaultVal = Utils.getCascadingPropertyValue(rb, propDefaultVal, "NULL");
+//			System.out.println("actualDefaultVal: " + actualDefaultVal);
+//			Assert.assertEquals("Expected default value.", propDefaultVal, actualDefaultVal);
+//			String actualConfigVal = Utils.getCascadingPropertyValue(rb, propDefaultVal, propKey);
+//			System.out.println("actualConfigVal: " + actualConfigVal);
+//			Assert.assertEquals("Expected configuration value.", propConfigVal, actualConfigVal);
+//			String actualSysVal = Utils.getCascadingPropertyValue(rb, propDefaultVal, propSysKey);
+//			System.out.println("actualSysVal: " + actualSysVal);
+//			Assert.assertEquals("Expected system value.", propSysVal, actualSysVal);
+
+			// Resource cleanup
+//			System.clearProperty(propKey);
+//			propsWriter.close();
+//			propsFile.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail("Exception caught.");
+		}
 	}
 
 	
