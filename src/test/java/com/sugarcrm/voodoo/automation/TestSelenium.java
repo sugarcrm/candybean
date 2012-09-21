635812155a995a92f886f9f9f2921e9418ea5391
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-09-21 13:27:31 -0700

    Renamed Selenium test to match; updating tests

--- src/test/java/com/sugarcrm/voodoo/automation/TestSelenium.java
+++ src/test/java/com/sugarcrm/voodoo/automation/TestSelenium.java
@@ -1,7 +1,7 @@
 package com.sugarcrm.voodoo.automation;
 
 
-public class TestSeleniumAutomation {
+public class TestSelenium {
 	
 	public void testStart() throws Exception {
 //		browser.get(url);
