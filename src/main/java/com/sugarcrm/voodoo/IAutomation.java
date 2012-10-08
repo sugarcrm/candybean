86f01035b8b48498f2c1f87e3c69b817ccdf8378
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-10-08 12:40:34 -0700

    alphabatizing api functions

--- src/main/java/com/sugarcrm/voodoo/IAutomation.java
+++ src/main/java/com/sugarcrm/voodoo/IAutomation.java
@@ -11,14 +11,20 @@ public interface IAutomation {
 
 	public void start(String url) throws Exception;
 	public void stop() throws Exception;
+	public void pause(long ms) throws Exception;
+	public void interact(String message) throws Exception;
+	public void acceptDialog() throws Exception;
+	public void switchToPopup() throws Exception;
 	public VControl getControl(VHook hook) throws Exception;
 	@Deprecated public VControl getControl(Strategy strategy, String hook) throws Exception;
-	public void click(VHook hook) throws Exception;
-	public void click(VControl control) throws Exception;
-	@Deprecated public void click(Strategy strategy, String hook) throws Exception;
 	public String getText(VHook hook) throws Exception;
 	public String getText(VControl control) throws Exception;
 	@Deprecated public String getText(Strategy strategy, String hook) throws Exception;
+	public void click(VHook hook) throws Exception;
+	public void click(VControl control) throws Exception;
+	@Deprecated public void click(Strategy strategy, String hook) throws Exception;
+    public void dragAndDrop(VHook hook1, VHook hook2) throws Exception;
+    public void dragAndDrop(VControl control1, VControl control2) throws Exception;
 	public void hover(VHook hook) throws Exception;
 	public void hover(VControl control) throws Exception;
 	@Deprecated public void hover(Strategy strategy, String hook) throws Exception;
@@ -30,10 +36,4 @@ public interface IAutomation {
 	@Deprecated public void rightClick(Strategy strategy, String hook) throws Exception;
 	public void scroll(VHook hook) throws Exception;
 	public void scroll(VControl control) throws Exception;
-	public void pause(long ms) throws Exception;
-	public void interact(String message) throws Exception;
-	public void acceptDialog() throws Exception;
-	public void switchToPopup() throws Exception;
-    public void dragAndDrop(VHook hook1, VHook hook2) throws Exception;
-    public void dragAndDrop(VControl control1, VControl control2) throws Exception;
 }
