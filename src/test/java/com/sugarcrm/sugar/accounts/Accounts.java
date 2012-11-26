c2ec54ff5ee92e8911a0a7346827e247159bdb37
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-11-26 11:41:26 -0800

    Portal test revision

--- src/test/java/com/sugarcrm/sugar/accounts/Accounts.java
+++ src/test/java/com/sugarcrm/sugar/accounts/Accounts.java
@@ -9,9 +9,9 @@ public class Accounts {
 	
 	public static void create(Voodoo voodoo, Sugar sugar, Account account) throws Exception {
 		(new VControl(sugar.getHook("navbar_menu_accounts"), voodoo.auto)).hover();
-		voodoo.pause(200);
+		(new VControl(sugar.getHook("navbar_menuitem_createaccount"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("navbar_menuitem_createaccount"), voodoo.auto)).click();
-		voodoo.pause(400);
+		(new VControl(sugar.getHook("accounts_textfield_name"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("accounts_textfield_name"), voodoo.auto)).hover();
 		(new VControl(sugar.getHook("accounts_textfield_name"), voodoo.auto)).sendString(account.name());
 		(new VControl(sugar.getHook("accounts_button_saveheader"), voodoo.auto)).click();
--- src/test/java/com/sugarcrm/sugar/contacts/Contacts.java
+++ src/test/java/com/sugarcrm/sugar/contacts/Contacts.java
@@ -1,7 +1,5 @@
 package com.sugarcrm.sugar.contacts;
 
-import javax.swing.JOptionPane;
-
 import com.sugarcrm.voodoo.automation.Voodoo;
 import com.sugarcrm.voodoo.automation.control.VControl;
 import com.sugarcrm.sugar.Sugar;
@@ -11,28 +9,27 @@ public class Contacts {
 	
 	public static void create(Voodoo voodoo, Sugar sugar, Contact contact) throws Exception {
 		(new VControl(sugar.getHook("navbar_menu_contacts"), voodoo.auto)).hover();
-		voodoo.pause(400);
+		(new VControl(sugar.getHook("navbar_menuitem_createcontact"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("navbar_menuitem_createcontact"), voodoo.auto)).click();
-		voodoo.pause(800);
+		(new VControl(sugar.getHook("contacts_textfield_lastname"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("contacts_textfield_lastname"), voodoo.auto)).hover();
 		(new VControl(sugar.getHook("contacts_textfield_lastname"), voodoo.auto)).sendString(contact.lastName());
 		(new VControl(sugar.getHook("contacts_button_accountselect"), voodoo.auto)).click();
 		voodoo.auto.focusByIndex(1);
+		(new VControl(sugar.getHook("accountsearch_textfield_name"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("accountsearch_textfield_name"), voodoo.auto)).sendString(contact.account().name());
 		(new VControl(sugar.getHook("accountsearch_button_search"), voodoo.auto)).click();
-		voodoo.pause(400);
+		(new VControl(sugar.getHook("accountsearch_link_firstitem"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("accountsearch_link_firstitem"), voodoo.auto)).click();
 		voodoo.auto.focusByIndex(0);
 		(new VControl(sugar.getHook("contacts_panel_portalinfo"), voodoo.auto)).scroll();
-		voodoo.pause(400);
+		(new VControl(sugar.getHook("contacts_textfield_portalname"), voodoo.auto)).waitOn();
 		(new VControl(sugar.getHook("contacts_textfield_portalname"), voodoo.auto)).sendString(contact.portalName());
 		if (contact.portalActive())	(new VControl(sugar.getHook("contacts_checkbox_portalactive"), voodoo.auto)).click();
 		(new VControl(sugar.getHook("contacts_password_portalpassword1"), voodoo.auto)).sendString(contact.portalPassword());
 		(new VControl(sugar.getHook("contacts_password_portalpassword2"), voodoo.auto)).sendString(contact.portalPassword());
 		(new VControl(sugar.getHook("contacts_div_bottomlinks"), voodoo.auto)).scroll();
 		(new VControl(sugar.getHook("contacts_button_savefooter"), voodoo.auto)).click();
-		voodoo.pause(400);
-		JOptionPane.showInputDialog("pause");
 	}
 	
 	public static void delete(Voodoo voodoo, Sugar sugar, Contact contact) throws Exception {
