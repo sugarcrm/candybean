f1639cc07b7eac6754aaaca750226bf47e57702e
Conrad Warmbold <cwarmbold@sugarcrm.com>
2012-11-27 01:13:28 -0800

    First pass forecasts tests; blocked by dev

--- src/test/java/com/sugarcrm/sugar/test/forecasts/ForecastsTest.java
+++ src/test/java/com/sugarcrm/sugar/test/forecasts/ForecastsTest.java
@@ -7,9 +7,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sugarcrm.sugar.SugarTest;
+import com.sugarcrm.sugar.admin.TimePeriod;
+import com.sugarcrm.sugar.admin.TimePeriods;
 
 
-public class Forecasts_10968 extends SugarTest {
+public class ForecastsTest extends SugarTest {
+	
+	private TimePeriod timePeriod;
 	
 	@BeforeClass
 	public static void setupOnce() throws Exception { SugarTest.setupOnce(); }
@@ -18,6 +22,8 @@ public class Forecasts_10968 extends SugarTest {
 	@Before
 	public void setup() throws Exception {
 		super.setup();
+		timePeriod = new TimePeriod("tp_new", true, "12/01/2012", "11/30/2013");
+		TimePeriods.create(voodoo, sugar, timePeriod);
 //		1. All default Time Periods are removed from the system 
 //		2. 5 custom time periods exist with the following data (YY represents the current year, MM represents the current month, 2M represents month MM+2, 3M = MM+3, 6M = MM+6)
 //        Name      -     Start Date       -       End Date     - Is Fiscal Year    -    Fiscal Year    - Forecast Start Date
@@ -29,15 +35,13 @@ public class Forecasts_10968 extends SugarTest {
 	}
 	
 	@Test
-	public void test() throws Exception {
+	public void test10968() throws Exception {
 		
 	}
 
 	@Override
 	@After
-	public void cleanup() throws Exception {
-		super.cleanup();
-	}
+	public void cleanup() throws Exception { super.cleanup(); }
 
 	@AfterClass
 	public static void cleanupOnce() { SugarTest.cleanupOnce(); }
