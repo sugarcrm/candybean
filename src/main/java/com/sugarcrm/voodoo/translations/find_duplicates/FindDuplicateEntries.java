464aa2132780e085b8636888465b533e5f0db8e5
Jason Lin <jason92lin@gmail.com>
2013-03-08 04:42:52 -0800

    Minor change.

--- src/main/java/com/sugarcrm/voodoo/translations/find_duplicates/FindDuplicateEntries.java
+++ src/main/java/com/sugarcrm/voodoo/translations/find_duplicates/FindDuplicateEntries.java
@@ -8,15 +8,17 @@ import com.sugarcrm.voodoo.utilities.Utils;
 
 public class FindDuplicateEntries {
 	private static ArrayList<String> MODULES;
-	private static ArrayList<String> ENTRIES;
+	private static ArrayList<String> EN_ENTRIES;
+	private static ArrayList<String> DUP_ENTRIES;
 
 	public static void main(String args[]) {
 		try {
 			Utils.connectToDB("10.8.31.10", "Translations_6_7_latest", "translator", "Sugar123!");
 			System.out.println("Successfully connected to database [add db_name parameter]");
 			MODULES = Utils.getTables();
-			ENTRIES = getAllENEntries(MODULES);
-			writeDuplicates(ENTRIES, "/var/lib/jenkins/DuplicateEntries.txt");
+			EN_ENTRIES = getAllENEntries(MODULES);
+			DUP_ENTRIES = getDupEntries(EN_ENTRIES);
+			writeDuplicates(DUP_ENTRIES, "/var/lib/jenkins/DuplicateEntries.txt");
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -30,7 +32,7 @@ public class FindDuplicateEntries {
 	public static ArrayList<String> getAllENEntries(ArrayList<String> modules) throws SQLException {
 		ArrayList<String> result = new ArrayList<String>();
 		ResultSet rs = null;
-	
+
 		for (String module : modules) {
 			rs = Utils.execQuery("SELECT Label, en_us FROM " + module);
 			while (rs.next()) {
@@ -48,34 +50,48 @@ public class FindDuplicateEntries {
 		return result;
 	}
 
-	private static void writeDuplicates(ArrayList<String> db_entries, String output_file) throws SQLException, IOException {
-		String last_label = null;
+	public static ArrayList<String> getDupEntries(ArrayList<String> en_entries) {
+		ArrayList<String> result = new ArrayList<String>();
 		String last_value = null;
-		String last_module = null;
+		Boolean added_last = false;
+
+		for (int i = 0; i < en_entries.size(); i++) {
+			if (i == 0) {
+				String[] s = en_entries.get(i).split("===", 3);
+				last_value = s[0];
+			}
+			String current_value = en_entries.get(i).split("===")[0];
+			if (i > 0 && last_value.equals(current_value)) {
+				if (!added_last) {
+					result.add("\n" + en_entries.get(i));
+					added_last = true;
+				}
+				result.add(en_entries.get(i));
+			} else {
+				last_value = current_value;
+				added_last = false;
+			}
+		}
+
+		return result;
+	}
+
+	private static void writeDuplicates(ArrayList<String> dup_entries, String output_file) throws SQLException, IOException {
+		String label = null;
+		String value = null;
+		String module = null;
 		BufferedWriter bw = new BufferedWriter(new FileWriter(output_file)); 
 		Boolean written_last = false;
 
-		for (int i = 0; i < db_entries.size(); i++) {
-			if (i == 0) {
-				String[] s = db_entries.get(i).split("===", 3);
-				last_label = s[2];
-				last_value = s[0];
-				last_module = s[1];
-			}
-			String current_label = db_entries.get(i).split("===")[2];
-			String current_value = db_entries.get(i).split("===")[0];
-			String current_module = db_entries.get(i).split("===")[1];
-			if (i > 0 && last_value.equals(current_value)) {
-				if (!written_last) {
-					bw.write("\nIn module " + last_module + ", " + last_label + "='" + last_value + "'\n");
-					written_last = true;
-				}
-				bw.write("In module " + current_module + ", " + current_label + "='" + current_value + "'\n");
+		for (int i = 0; i < dup_entries.size(); i++) {
+			String[] s = dup_entries.get(i).split("===", 3);
+			label = s[2];
+			value = s[0];
+			module = s[1];
+			if (value.contains("\n")) {
+				bw.write("\nIn module " + module + ", " + label + "='" + value + "'\n");
 			} else {
-				last_label = current_label;
-				last_value = current_value;
-				last_module = current_module;
-				written_last = false;
+				bw.write("In module " + module + ", " + label + "='" + value + "'\n");
 			}
 		}
 		bw.close();
