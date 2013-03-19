21f68b64bdb8ce1176fec023deac4f31c8bd245e
jason92lin@gmail.com <jason92lin@gmail.com>
2013-03-18 18:37:57 -0700

    Fixed bugs in FindLocationsOfAPattern.java

--- src/main/java/com/sugarcrm/voodoo/translations/findduplicates/FindLocationsOfAPattern.java
+++ src/main/java/com/sugarcrm/voodoo/translations/findduplicates/FindLocationsOfAPattern.java
@@ -1,11 +1,13 @@
 package com.sugarcrm.voodoo.translations.findduplicates;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class FindLocationsOfAPattern {
+	private static int fileNumber = 0;
 
 	/**
 	 * @param args
@@ -14,8 +16,17 @@ public class FindLocationsOfAPattern {
 		String patternsPath = args[0];
 		String testPath = args[1];
 		String outputPath = args[2];
+		String listOfPatternsPath = args[3];	
 		try {
 			findAllPatterns(patternsPath, testPath, outputPath);
+			int lineNumber = 0;
+			Scanner s = new Scanner(new File(patternsPath));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outputPath + File.separator + listOfPatternsPath)));
+			while (s.hasNext()) {
+				lineNumber++;
+				bw.write(lineNumber + ". " + s.nextLine().trim() + "\n");
+			}
+			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -41,15 +52,13 @@ public class FindLocationsOfAPattern {
 		File inputFile = new File(testPath);
 
 		if (inputFile.isFile() && inputFile.getName().contains(".xml") && foundPatternInFile(pattern, testPath)) {
-			//String tmp = pattern.substring(11);
-			//String fileName = tmp.substring(0, tmp.length() - 3);
-			File outputFile = new File(outputPath + File.separator + pattern.substring(11).substring(0, pattern.length() - 13));
+			File outputFile = new File(outputPath + File.separator + fileNumber);
 			File outputFileParent = outputFile.getParentFile();
 			if (!outputFileParent.exists())
 				outputFileParent.mkdirs();
-			FileWriter fw = new FileWriter(outputFile);
-			fw.append(testPath);
-			fw.close();
+			BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile, true));
+			bw.write(testPath + "\n");
+			bw.close();
 		} else if (inputFile.isDirectory()) {
 			File[] files = inputFile.listFiles();
 			for (File file : files) {
@@ -62,6 +71,7 @@ public class FindLocationsOfAPattern {
 		Scanner s = new Scanner(new File(patternsPath));
 
 		while (s.hasNext()) {
+			fileNumber++;
 			String pattern = s.nextLine().trim();
 			File file = new File(outputPath);
 			File parent = file.getParentFile();
