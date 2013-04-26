5097200f75c202d294624aec1cd73518df31201e
Jason Lin <jason92lin@gmail.com>
2013-04-26 16:39:30 -0700

    Finalized validator. Modified FindSuitableTests methods to work with Suga user on the VM.

--- src/main/java/com/sugarcrm/voodoo/translations/validator/validator.php
+++ src/main/java/com/sugarcrm/voodoo/translations/validator/validator.php
@@ -9,6 +9,9 @@
     <h1>Translation Validator</h1>
     <p>Validates whether an English string is suitable for translation.</p>
     <p>Lists all translations of the input string in the selected language.</p>
+    <p>When using an English string in a SodaIceBox test, use this tool to check if the string is a candidate for translations. If the string is, using it means the resulting test can be translated to another language and be fully functional.</p>
+    <p>Validating the string returns either: 1) the string is valid (go ahead and use it!) or 2) a list of different translations that exist for this string (consider using another string!)</p>
+    <p>View translations can be used to see translations for an English string in a specific language</p>
     <form action="validator_results.php" method="post">
       English: <input type="text" name="english"/><br/>
       <input type="submit" name="validate" value="Validate"/><br/><br/>
