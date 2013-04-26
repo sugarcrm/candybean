d3c4ccfbe15619ba20fc6e33b657fc4164174544
Jason Lin <jason92lin@gmail.com>
2013-04-26 15:39:26 -0700

    Finalized translatetests and validator in translations.

--- src/main/java/com/sugarcrm/voodoo/translations/validator/validator.php
+++ src/main/java/com/sugarcrm/voodoo/translations/validator/validator.php
@@ -9,11 +9,9 @@
     <h1>Translation Validator</h1>
     <p>Validates whether an English string is suitable for translation.</p>
     <p>Lists all translations of the input string in the selected language.</p>
-    <p>If the number of English instances is 0, translation does not exist.</p>
-    <p>If the number of English instances exceeds 1, translation may be ambiguous (2+ unique mappings).*</p>
-    <p><small>	*Porting an existing feature to PHP that informs the user if the translation is in fact ambiguous.</small></p>
     <form action="validator_results.php" method="post">
       English: <input type="text" name="english"/><br/>
+      <input type="submit" name="validate" value="Validate"/><br/><br/>
       Language: <select name="language">
 <?php
   include_once "language.php";
@@ -22,9 +20,8 @@
     echo "      <option value=\"$code\">$native</option>\n";
   }
 ?>
-      </select><br/><br/>
-      <input type="submit" name="translate" value="View translations"/><br/>
-      <input type="submit" name="validate" value="Validate"/><br/>
+      </select><br/>
+      <input type="submit" name="translate" value="View translations"/><br/> 
     </form>
   </body>
 </html>
