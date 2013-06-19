ad69235fbeed87c02167224e9925ba30503d4983
sqwerl <larry.cao@gmail.com>
2013-06-18 19:48:54 -0700

    update iOS test

--- src/test/java/com/sugarcrm/voodoo/automation/mobile/SugarTest.java
+++ src/test/java/com/sugarcrm/voodoo/automation/mobile/SugarTest.java
@@ -12,17 +12,11 @@ import com.sugarcrm.voodoo.datasource.DS;
 import com.sugarcrm.voodoo.datasource.DataSource;
 
 
-
-/**
- * Created with IntelliJ IDEA.
- * User: lcao
- * Date: 6/12/13
- * Time: 5:09 PM
- * To change this template use File | Settings | File Templates.
- */
 public class SugarTest {
 
     protected static VoodooUtils utils;
+
+
 
 
 
