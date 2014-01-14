121cdca7f83b16e7b090eb15da7bfdc1b0838f27
Conrad Warmbold <cwarmbold@sugarcrm.com>
2014-01-14 00:38:54 -0800

    NITs

--- src/test/java/com/sugarcrm/candybean/automation/mobile/AndroidSugarTest.java
+++ src/test/java/com/sugarcrm/candybean/automation/mobile/AndroidSugarTest.java
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.sugarcrm.candybean.android;
+package com.sugarcrm.candybean.automation.mobile;
 
 //import java.io.File;
 //import java.io.FileInputStream;
