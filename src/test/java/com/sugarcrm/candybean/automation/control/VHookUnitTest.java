a6c2c9ad97bd9039bdc8a919e8df01f322afd319
Ownageful <sfarooq@sugarcrm.com>
2014-01-21 14:07:00 -0800

    YelpTest.java and several other test classes now use AbstractTest.java, and removed unnecessary CB.java class

--- src/test/java/com/sugarcrm/candybean/automation/control/VHookUnitTest.java
+++ src/test/java/com/sugarcrm/candybean/automation/control/VHookUnitTest.java
@@ -29,9 +29,9 @@ import java.util.Properties;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.sugarcrm.candybean.CB;
 import com.sugarcrm.candybean.automation.control.VHook;
 import com.sugarcrm.candybean.automation.control.VHook.Strategy;
+import com.sugarcrm.candybean.test.AbstractTest;
 
 
 public class VHookUnitTest {
@@ -39,7 +39,7 @@ public class VHookUnitTest {
 	@Test
 	public void testGetHooks() {
 		try {
-			File hooksFile = new File(CB.CONFIG_DIR.getCanonicalPath() + File.separator + "testutils.hooks");
+			File hooksFile = new File(AbstractTest.CONFIG_DIR.getCanonicalPath() + File.separator + "testutils.hooks");
 			hooksFile.createNewFile();
 			Properties hooksProps = new Properties();
 			String hook1Name = "hook1name";
