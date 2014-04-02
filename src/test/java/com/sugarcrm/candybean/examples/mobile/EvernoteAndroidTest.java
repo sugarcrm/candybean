cf8ff87fa381d528f28fe1de20041e5f4a0ff2af
Ownageful <sherry.farooq@hotmail.com>
2014-04-02 16:42:31 -0700

    First iteration for autoamted mobile tests

--- src/test/java/com/sugarcrm/candybean/examples/mobile/EvernoteAndroidTest.java
+++ src/test/java/com/sugarcrm/candybean/examples/mobile/EvernoteAndroidTest.java
@@ -81,10 +81,7 @@ public class EvernoteAndroidTest {
 		noteField
 				.sendKeys("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor "
 						+ "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
-						+ "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
-						+ " dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-						+ "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit "
-						+ "anim id est laborum");
+						+ "exercitation ullamco laboris nis");
 		iface.wd.findElement(By.className("android.widget.ImageButton")).click();
 		iface.pause(2000);
 	}
