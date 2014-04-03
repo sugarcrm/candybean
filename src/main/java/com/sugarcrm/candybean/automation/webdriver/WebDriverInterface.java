f0b014898594a84e5996888e2cda1db6bfa8bef7
Ownageful <sherry.farooq@hotmail.com>
2014-04-03 14:28:48 -0700

    fix for mac environment 2

--- src/main/java/com/sugarcrm/candybean/automation/webdriver/WebDriverInterface.java
+++ src/main/java/com/sugarcrm/candybean/automation/webdriver/WebDriverInterface.java
@@ -89,9 +89,9 @@ public abstract class WebDriverInterface extends AutomationInterface {
 				&& !(this instanceof AndroidInterface)
 				&& !(this instanceof IosInterface)
 				&& !System.getProperty("os.name").contains("mac")) {
-			java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-			wd.manage().window().setSize(new Dimension(screenSize.width, screenSize.height));
-			this.windows.push(new Pair<Integer, String>(new Integer(0), this.wd.getWindowHandle()));
+			//java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			//wd.manage().window().setSize(new Dimension(screenSize.width, screenSize.height));
+			//this.windows.push(new Pair<Integer, String>(new Integer(0), this.wd.getWindowHandle()));
 		}
 	}
 	
